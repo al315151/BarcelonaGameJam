f2fb058d2ec9f82dd45c55f7e90229763d00d492
Jonatan <al315616@uji.es>
2018-11-24 21:16:16 +0100

    Once
    Super Hexagon que no es super Hexagon

---
+++ Assets/Jonatan/Scripts Jonatan/ScriptsMiniGame2/MG2BulletMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG2BulletMovement : MonoBehaviour
+{
+    float speed = 10;
+
+    // Start is called before the first frame update
+
+
+    private void FixedUpdate()
+    {
+        this.gameObject.transform.Translate(0, speed * Time.deltaTime, 0);
+
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(this.transform.position);
+        if ((screenPoint.x < 0 || screenPoint.x > 1) || (screenPoint.y < 0 || screenPoint.y > 1))
+        {
+
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Jonatan/Scripts Jonatan/ScriptsMiniGame2/MG2PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG2PlayerMovement : MonoBehaviour
+{
+    public float speed = 700f;
+    public GameObject bullet;
+    public Transform bulletExit;
+    float horizontalMovement = 0f;
+    float shootRate = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        shootRate -= Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space)) {
+
+            if (shootRate <= 0) {
+
+                Shoot();
+
+            }
+            
+
+        }
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, horizontalMovement * Time.fixedDeltaTime * -speed);
+    }
+
+
+    void Shoot() {
+
+
+        Instantiate(bullet, bulletExit.position, this.gameObject.transform.rotation);
+        shootRate = 0.1f;
+    }
+}
