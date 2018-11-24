f6d1323bc02736a782481e9d98f0fcb8b965bf42
Jonatan <al315616@uji.es>
2018-11-24 13:14:47 +0100

    Guitar Hero Game Works

---
+++ Assets/Jonatan/Scripts Jonatan/KeyGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyGenerator : MonoBehaviour
+{
+    public GameObject[] KeysList = new GameObject[4]; 
+    public float timer = 0.5f;
+    int keysCounter = 0;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (keysCounter < 20) {
+
+
+            if (timer <= 0)
+            {
+                Random random = new Random();
+                int i = Random.Range(0, KeysList.Length);
+                Instantiate(KeysList[i]);
+                timer = 1f;
+                keysCounter += 1;
+            }
+            else
+            {
+
+                timer -= Time.deltaTime;
+
+            }
+
+
+
+        }
+       
+    }
+}
---
+++ Assets/Jonatan/Scripts Jonatan/PlayData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayData : MonoBehaviour
+{
+    public static PlayData instance;
+    int points = 0;
+    int streak = 0;
+    float streakMultiplier = 0.75f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void Hit() {
+
+        streak += 1;
+        points = points + (int)(streak/4 * streakMultiplier);
+        print("Points = " + points + " ; Streak = " + streak);
+
+
+    }
+
+    public void Fail() {
+
+        streak = 0;
+        print("Points = " + points + " ; Streak = " + streak);
+
+    }
+}
