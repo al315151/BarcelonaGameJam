c68bcfe09927583e4b056677ab0b5199c11c6d8f
car1l2os <al315149@uji.es>
2018-11-25 17:33:15 +0100

    aa

---
+++ Assets/ControlPlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayerInput : MonoBehaviour
+{
+    public void EnableInput()
+    {
+        UnityStandardAssets.Characters.FirstPerson.FirstPersonController.canInput = true;
+    }
+
+    public void DisableInput()
+    {
+        UnityStandardAssets.Characters.FirstPerson.FirstPersonController.canInput = false;
+    }
+}
