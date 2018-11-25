0ca93eb9327ffad9711a126ce136d534e9eb9f45
Olga Montero <olggamonter@gmail.com>
2018-11-25 02:46:07 +0100

    Pong

---
+++ Assets/Carlos/Scripts/BolaPongBehaviour.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BolaPongBehaviour : MonoBehaviour
+{
+    Vector2 dir;
+    float speed = 5;
+    private SpriteRenderer sprite;
+    public Transform maxPoint;
+    public Transform minPoint;
+    public PalaController p1;
+    public PalaController p2;
+    public Text p1TextScore;
+    public Text p2TextScore;
+    private int p1Score;
+    private int p2Score;
+    private int maxScore = 3;
+    private float timer = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        dir = new Vector2(Random.value * 2 - 1, Random.value - 0.5f);
+        p1Score = 0;
+        p2Score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer<=0)
+        {
+            dir *= 1.1f;
+            timer += 1.0f;
+        }
+
+        transform.Translate(dir * speed * Time.deltaTime);
+        if (transform.position.y + sprite.sprite.bounds.size.y / 2 * transform.localScale.y > maxPoint.position.y || transform.position.y - sprite.sprite.bounds.size.y / 2 * transform.localScale.y < minPoint.position.y)
+            dir = new Vector2(dir.x, -dir.y);
+        if (transform.position.x + sprite.sprite.bounds.size.x / 2 * transform.localScale.x > maxPoint.position.x)
+        {
+            //sale por la derecha
+            p1Score++;
+            ResetGame();
+            p1TextScore.text = p1Score.ToString();
+            if(p1Score == maxScore)
+            {
+                //gana p1
+            }
+        }
+        else if(transform.position.x - sprite.sprite.bounds.size.x / 2 * transform.localScale.x < minPoint.position.x)
+        {
+            //sale por la izquierda
+            p2Score++;
+            ResetGame();
+            p2TextScore.text = p2Score.ToString();
+            if (p2Score == maxScore)
+            {
+                //gana p2
+            }
+
+        }
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PalaPong")
+            dir = new Vector2(-dir.x, dir.y);
+    }
+
+    private void ResetGame()
+    {
+        transform.position = Vector3.zero;
+        p1.ResetPos();
+        p2.ResetPos();
+        dir = new Vector2(Random.value * 2 - 1, Random.value - 0.5f);
+    }
+}
---
+++ Assets/Carlos/Scripts/PalaController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PalaController : MonoBehaviour
+{
+    private float playerSpeed = 10;
+    private float iaSpeed = 3;
+    private SpriteRenderer sprite;
+    public Transform maxPoint;
+    public Transform minPoint;
+    public bool IAControlled;
+    public Transform ball;
+    private Vector3 originalPos;
+
+    private void Awake()
+    {
+        originalPos = transform.position;
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 previousPos = transform.position;
+        if (!IAControlled)
+        {
+            if (Input.GetKey(KeyCode.W))
+                Move(Vector3.up,playerSpeed);
+            if (Input.GetKey(KeyCode.S))
+                Move(Vector3.down,playerSpeed);
+        }
+        else
+        {
+            if (ball.position.y > transform.position.y)
+                Move(Vector3.up, iaSpeed);
+            else if(ball.position.y < transform.position.y)
+                Move(Vector3.down, iaSpeed);
+        }
+    }
+
+    public void Move(Vector3 dir, float speed)
+    {
+        Vector3 previousPos = transform.position;
+        transform.Translate(dir * speed * Time.deltaTime);
+        if (transform.position.y + sprite.sprite.bounds.size.y / 2 * transform.localScale.y > maxPoint.position.y || transform.position.y - sprite.sprite.bounds.size.y / 2 * transform.localScale.y < minPoint.position.y)
+            transform.position = previousPos;
+    }
+
+    public void ResetPos()
+    {
+        transform.position = originalPos;
+    }
+}
