d8679902a6e74809ee0e175822535aa43daa0cdd
Jonatan <al315616@uji.es>
2018-11-25 15:39:55 +0100

    THE WORLD
    +Arreglo laberinto

---
+++ Assets/Jonatan/Scripts Jonatan/ScriptsMiniGame2/MG2Loop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG2Loop : MonoBehaviour
+{
+    public MG2Loop instance;
+    public bool death;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
