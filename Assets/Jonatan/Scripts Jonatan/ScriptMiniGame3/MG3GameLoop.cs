904e8d8299f13df3d9f14e3deb5439796e0206ad
Jonatan <al315616@uji.es>
2018-11-25 17:42:52 +0100

    Gonna
    Laberinto con ciclo

---
+++ Assets/Jonatan/Scripts Jonatan/ScriptMiniGame3/MG3GameLoop.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MG3GameLoop : MonoBehaviour
+{
+    bool end = false;
+    public GameObject player;
+    GameObject aux;
+    public Text victory;
+
+    private void Start()
+    {
+        aux = Instantiate(player);
+    }
+
+    private void Update()
+    {
+        if (aux.gameObject.transform.position.y <= -1) {
+
+            victory.text = "Has escapado";
+            end = true;
+
+
+        }
+
+
+        if (end) {
+
+            if (Input.GetKeyDown(KeyCode.R)) {
+
+                victory.text = "";
+                Destroy(aux);
+                aux = Instantiate(player);
+                end = false;
+            }
+        }
+    }
+
+
+
+}
