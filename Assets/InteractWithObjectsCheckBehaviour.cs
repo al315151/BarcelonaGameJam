6466a94bc1632e99d482e7c199d06046a4eefeba
Olga Montero <olggamonter@gmail.com>
2018-11-25 12:02:16 +0100

    Interact

---
+++ Assets/InteractWithObjectsCheckBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWithObjectsCheckBehaviour : MonoBehaviour
+{
+    public GameObject interactUI;
+    private RaycastHit hit;
+    private float checkDistance = 5f;
+    int layerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        layerMask = 1 << 10;
+        interactUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward)* checkDistance, Color.red, 0.1f);
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, checkDistance, layerMask))
+        {
+            interactUI.SetActive(true);
+        }
+        else
+            interactUI.SetActive(false);
+    }
+}
